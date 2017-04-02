8b709fe4b36c5cbe504ff668c70bdacd2f88035b
tomgozdek <tomgozdek@gmail.com>
2017-04-02 11:17:42 +0200

    Add model representing item returned by API

---
+++ app/src/main/java/com/tomekgozdek/futureapp/model/FutureItem.java
@@ -0,0 +1,66 @@
+package com.tomekgozdek.futureapp.model;
+
+import java.util.Date;
+
+/**
+ * Model class representing FutureItem object retrieved from API.
+ *
+ * Created by TomekG on 2017-04-02.
+ */
+
+public class FutureItem {
+    private String title;
+    private String description;
+    private int orderId;
+    private String url;
+    private String imageUrl;
+    private Date date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
