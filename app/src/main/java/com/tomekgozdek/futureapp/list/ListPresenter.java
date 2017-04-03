7cc57145104af9a49f3fc9bf646019ab51c214f7
tomgozdek <tomgozdek@gmail.com>
2017-04-03 08:57:30 +0200

    Add logic for gathering data to ListPresenter.

--- app/src/main/java/com/tomekgozdek/futureapp/list/ListPresenter.java
+++ app/src/main/java/com/tomekgozdek/futureapp/list/ListPresenter.java
@@ -1,10 +1,19 @@
 package com.tomekgozdek.futureapp.list;
 
+import com.tomekgozdek.futureapp.api.ApiClient;
+import com.tomekgozdek.futureapp.api.ApiResponse;
+import com.tomekgozdek.futureapp.api.FutureService;
 import com.tomekgozdek.futureapp.model.FutureItem;
 import com.tomekgozdek.futureapp.presenter.BasicView;
 import com.tomekgozdek.futureapp.presenter.Presenter;
+import com.tomekgozdek.futureapp.repository.FutureRealmRepository;
+import com.tomekgozdek.futureapp.repository.FutureRealmRepositoryImpl;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by TomekG on 2017-04-03.
@@ -19,6 +28,45 @@ public class ListPresenter implements Presenter {
     }
     @Override
     public void onResume() {
+        downloadItems();
+    }
+
+    private void downloadItems() {
+        FutureService service = ApiClient.getApiClient().create(FutureService.class);
+
+        Call<ApiResponse> request = service.listFutureItems();
+        request.enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                if(response.isSuccessful()){
+                    storeItems(response.body().getData());
+                } else {
+                    //TODO handle failure case
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                //TODO handle failure case
+            }
+        });
+    }
+
+    private void storeItems(List<FutureItem> list) {
+        FutureRealmRepository repository = new FutureRealmRepositoryImpl();
+        repository.storeFutureItemsAsync(list, new FutureRealmRepository.RepositoryCallback() {
+            @Override
+            public void onTransactionCompleted(boolean success) {
+                if(success) {
+                    loadItemsFromRepository();
+                }
+            }
+        });
+    }
+
+    private void loadItemsFromRepository(){
+        FutureRealmRepository repository = new FutureRealmRepositoryImpl();
+        mView.loadItems(repository.getFutureItems());
     }
 
     interface View extends BasicView<ListPresenter>{
